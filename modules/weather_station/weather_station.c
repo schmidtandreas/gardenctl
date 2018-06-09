a4b7f93b4e9323e7c135add1bd5bc82f4f67a160
Andreas Schmidt <mail@schmidt-andreas.de>
2018-06-09 18:19:39 +0200

    add weather_station
    Signed-off-by: Andreas Schmidt <mail@schmidt-andreas.de>

---
+++ modules/weather_station/weather_station.c
@@ -0,0 +1,203 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <config.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <garden_module.h>
+#include <garden_common.h>
+#include <logging.h>
+#include <gpioex.h>
+
+#define INTERVAL_SEC 30
+
+#define DHT_TEMP_FILENAME "/sys/bus/iio/devices/iio:device0/in_temp_input"
+#define DHT_HUMREL_FILENAME "/sys/bus/iio/devices/iio:device0/in_humidityrelative_input"
+
+enum thread_state {
+	THREAD_STATE_STOPPED,
+	THREAD_STATE_RUNNING,
+};
+
+struct weather_station {
+	pthread_t thread;
+	pthread_mutex_t lock;
+	enum thread_state thread_state;
+};
+
+static int get_dht_temp(double *temp)
+{
+	int ret = 0;
+	int fd;
+	char buffer[20] = {0};
+	*temp = 0.0;
+
+	ret = open(DHT_TEMP_FILENAME, O_RDONLY);
+	if (ret < 0)
+		goto out;
+
+	fd = ret;
+
+	ret = read(fd, buffer, sizeof(buffer));
+	if (ret < 0)
+		goto out_close;
+
+	*temp = atoi(buffer);
+	*temp /= 1000;
+
+	log_dbg("dht temp: %.1f", *temp);
+out_close:
+	close(fd);
+out:
+	return ret;
+}
+
+static int get_dht_hum(double *hum)
+{
+	int ret = 0;
+	int fd;
+	char buffer[20] = {0};
+	*hum = 0.0;
+
+	ret = open(DHT_HUMREL_FILENAME, O_RDONLY);
+	if (ret < 0)
+		goto out;
+
+	fd = ret;
+
+	ret = read(fd, buffer, sizeof(buffer));
+	if (ret < 0)
+		goto out_close;
+
+	*hum = atoi(buffer);
+	*hum /= 1000;
+
+	log_dbg("dht hum: %.1f", *hum);
+out_close:
+	close(fd);
+out:
+	return ret;
+}
+
+static void* gm_thread_run(void *obj)
+{
+	struct garden_module *gm = (struct garden_module*) obj;
+	struct weather_station *data = (struct weather_station*) gm->data;
+	enum thread_state state;
+	double dht_temp = 0;
+	double dht_hum = 0;
+
+	state = data->thread_state = THREAD_STATE_RUNNING;
+
+	while (state == THREAD_STATE_RUNNING) {
+		int ret = 0;
+		double curr_dht_temp, curr_dht_hum;
+
+		ret = get_dht_temp(&curr_dht_temp);
+		if (ret >= 0 && dht_temp != curr_dht_temp) {
+			char buf[10] = {0};
+			dht_temp = curr_dht_temp;
+
+			sprintf(buf, "%.1f", dht_temp);
+
+			ret = mosquitto_publish(gm->mosq, NULL, "/garden/sensor/temperature",
+						strlen(buf), buf, 2, false);
+			if (ret < 0) {
+				log_err("publish dht temperature failed (%d) %s", ret,
+					mosquitto_strerror(ret));
+			}
+		}
+
+		ret = get_dht_hum(&curr_dht_hum);
+		if (ret >= 0 && dht_hum != curr_dht_hum) {
+			char buf[10] = {0};
+			dht_hum = curr_dht_hum;
+
+			sprintf(buf, "%.1f", dht_hum);
+
+			ret = mosquitto_publish(gm->mosq, NULL, "/garden/sensor/humidity",
+						strlen(buf), buf, 2, false);
+			if (ret < 0) {
+				log_err("publish dht humidity failed (%d) %s", ret,
+					mosquitto_strerror(ret));
+			}
+		}	
+
+		sleep(INTERVAL_SEC);
+		pthread_mutex_lock(&data->lock);
+		state = data->thread_state;
+		pthread_mutex_unlock(&data->lock);
+	}
+
+	return NULL;
+}
+
+static void gm_thread_stop(struct weather_station* data)
+{
+	int ret = 0;
+
+	if (data) {
+		pthread_mutex_lock(&data->lock);
+		data->thread_state = THREAD_STATE_STOPPED;
+		pthread_mutex_unlock(&data->lock);
+
+		pthread_join(data->thread, NULL);
+	}
+}
+
+static int gm_init(struct garden_module *gm, struct mosquitto* mosq)
+{
+	int ret = 0;
+	struct weather_station *data = NULL;
+
+	gm->mosq = mosq;
+	gm->data = malloc(sizeof(struct weather_station));
+
+	if (!gm->data) {
+		log_err("allocation for weather station data failed");
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	data = (struct weather_station*)gm->data;
+
+	ret = pthread_mutex_init(&data->lock, NULL);
+	if (ret) {
+		log_err("initiate weather station lock failed (%d) %s", ret, strerror(ret));
+		goto out;
+	}
+
+	ret = pthread_create(&data->thread, NULL, &gm_thread_run, gm);
+	if (ret) {
+		log_err("create weather station thread failed (%d) %s", ret, strerror(ret));
+		goto out;
+	}
+out:
+	return ret;
+}
+
+struct garden_module *create_garden_module(enum loglevel loglevel)
+{
+	struct garden_module *module = malloc(sizeof(*module));
+
+	if (module) {
+		memset(module, 0, sizeof(*module));
+		module->init = gm_init;
+
+		max_loglevel = loglevel;
+	}
+
+	return module;
+}
+
+void destroy_garden_module(struct garden_module *module)
+{
+	if (module) {
+		gm_thread_stop(module->data);
+		if (module->data)
+			free(module->data);
+		free(module);
+	}
+}
